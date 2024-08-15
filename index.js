a314d927819d12fd9cbff37f0e3308cad1594509
Kananek Thongkam <info.dvgamer@gmail.com>
2024-08-15 11:38:20 +0700

    refactor: recogination with tesseract

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-import * as mupdf from "mupdf"
 
-const filename = './pdfdir./ครั้งที่ 3 (พฤ.8 ต.ค.58).pdf'
+import { createWorker } from 'tesseract.js'
+import * as mupdf from "mupdf"
+import pino from 'pino'
+
+const logger = pino({ level: Bun.env.DEBUG || 'info' })
+const filename = './pdfdir/ครั้งที่ 3 (พฤ.8 ต.ค.58).pdf'
 const file = Bun.file(filename)
 const buffer = await file.arrayBuffer()
 const doc = mupdf.Document.openDocument(buffer, "application/pdf")
 
+const zoom = 2.2
+
 let i = 0
 // while (i < doc.countPages()) {
-  const page = doc.loadPage(i)
-  console.log(mupdf.Matrix.identity)
-  console.log(mupdf.ColorSpace.DeviceGray)
-
-  let pixmap = page.toPixmap(mupdf.Matrix.scale(2.2, 2.2), mupdf.ColorSpace.DeviceGray, false, true)
-  let pngImage = pixmap.asPNG()
-  await Bun.write(`./pages/${i}.png`, Buffer.from(pngImage, 'binary'))
-  i++
+const page = doc.loadPage(i)
+const pixmap = page.toPixmap(mupdf.Matrix.scale(zoom, zoom), mupdf.ColorSpace.DeviceGray, false, true)
+await Bun.write(`./pdfdir/pages/${i}.png`, Buffer.from(pixmap.asPNG(), 'binary'))
 // }
+
+const worker = await createWorker('tha', 1);
+
+logger.info('Processing')
+
+const { data: { text, hocr } } = await worker.recognize(`./pdfdir/pages/${i}.png`);
+
+await Bun.write(`./pdfdir/pages/${i}.text.txt`, text)
+await Bun.write(`./pdfdir/pages/${i}.hocr.txt`, hocr)
+await worker.terminate();
+
+logger.info('Finish')
